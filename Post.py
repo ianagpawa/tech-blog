f31a84871a39ab18ae1e6f6b7a9bd6b6c633f9bb
Ian Agpawa <agpawaji@gmail.com>
2017-06-10 23:42:23 -0400

    feat: Add Post model

---
+++ Post.py
@@ -0,0 +1,49 @@
+import cgi
+import textwrap
+import urllib
+
+from google.appengine.ext import ndb
+
+class Post(ndb.Model):
+    """
+    This class is for blog posts
+    Attributes:
+        title (str):  Title of post.
+        content (str): Content of the post.
+        creator (str): Author of the post.
+        created (date): Date of when the blog post was created.
+        last_modified (date): Date of when the blog post was last modified.
+    """
+
+
+    title = ndb.StringProperty(required = True)
+    content = ndb.TextProperty(required = True)
+    creator = "Ian Agpawa"
+    created = ndb.DateTimeProperty(auto_now_add = True)
+    last_modified = ndb.DateTimeProperty(auto_now = True)
+
+
+    def rendered_content(self):
+        """
+        rendered_content:  Method for retaining the multiline format of the
+                            blogpost.
+
+        Returns:
+            String of blog post content with newlines replaced with html breaks.
+        """
+        return self.content.replace("\n", "<br>")
+
+
+    def render_back(self):
+        """
+        render_back:  Method for retaining the multiline format of the
+                        blogpost after it has been retrieved from the database.
+        Returns:
+            String of blog post content with html breaks replaced with newlines.
+        """
+        return self.content.replace("<br>", "\n")
+
+
+
+def blog_key(name = 'default'):
+    return ndb.Key.from_path('/', name)
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, redirect, url_for, flash, \
+                    make_response, jsonify
+#   imports for authentication
+from flask import session as login_session
+import random
+import string
+
+import json
+from functools import wraps
+
+import Post
+
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
 

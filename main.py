3cadb21a183e1f9b3c76cecade7e42e582f57c3f
Ian Agpawa <agpawaji@gmail.com>
2017-06-11 21:11:05 -0400

    feat: Add login session and logout handler"

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 from flask import Flask, render_template, request, redirect, url_for, flash, \
-                    make_response, jsonify
+                    make_response, jsonify, session, escape
 #   imports for authentication
-from flask import session as login_session
 import random
 import string
 import hashlib
+import hmac
 from string import letters
 
 
@@ -19,11 +19,14 @@ from User import User
 from google.appengine.ext import ndb
 
 app = Flask(__name__)
-
+app.secret_key = 'IsThatYouJohnWayne?'
 
 
 @app.route('/')
 def front():
+    if 'username' in session:
+        username = session['username']
+        print username
     posts = Post.query()
     return render_template("front.html", posts=posts)
 
@@ -44,77 +47,8 @@ def newPost():
 
 
 
-
-phrase = "DontYourPutThatEvilOnMeRickyBobby"
-def make_secure(unsecured):
-    """
-    make_secure: method for creating a secure password keyed-hash using HMAC
-                and a pass phrase.
-    Args:
-        unsecured (data type: str): string input of user's unsecure password
-    Returns:
-        returns concatenated string of HMAC keyed-hash password and
-            unsecured password
-    """
-    return '%s|%s' % (hmac.new(phrase, unsecured).hexdigest(), unsecured)
-
-
-
-def check_secure(secured_pass):
-    """
-    make_secure: method for creating a secure password keyed-hash using HMAC
-                and a pass phrase.
-    Args:
-        secured_pass (data type: str): string input of user's hashed password
-    Returns:
-        True if the secured_pass matches the unsecured password substring after
-        it has been hashed.
-    """
-    unsecured = secured_pass.split('|')[1]
-    if secured_pass == make_secure(unsecured):
-        return unsecured
-
-
-
-
 @app.route("/login/", methods=["GET", 'POST'])
 def login():
-
-
-    def set_secure_cookie(self, name, value):
-        """
-        set_secure_cookie:  Method for setting a secure cookie.
-        Args:
-            name (data type: str): Name of a cookie.
-            value (data type: str): Value of a cookie
-        """
-        secured_cookie = make_secure(value)
-        return response.headers.add_header(
-            'Set-Cookie',
-            "%s=%s; Path=/" % (name, secured_cookie)
-        )
-
-
-    def read_secure_cookie(self, name):
-        """
-        read_secure_cookie:     Method for reading a secure cookie.
-        Args:
-            name (data type: str): Name of a cookie
-        Returns:
-            True or False
-        """
-        secured_cookie = request.cookies.get(name)
-        return secured_cookie and check_secure(secured_cookie)
-
-    def login(self, user):
-        """
-        login:  Method for logging a user in.
-        Args:
-            user_id (data type: str):   The user key id.
-        """
-        user_id = str(user.key().id())
-        return self.set_secure_cookie('user_id', user_id)
-
 
     if request.method == "POST":
         username = request.form["username"]
@@ -122,27 +56,29 @@ def login():
         user = User.query()
         user = user.filter(User.name == username).fetch()
 
-        print user[0]
         if len(user) > 0:
-            name = user[0].name
-            pass_hash = user[0].pass_hash
-
+            user = user[0]
+            name = user.name
+            pass_hash = user.pass_hash
             if username == name and valid_pass(name, password, pass_hash):
-                return redirect("/")
+                session['username'] = username
+                return redirect(url_for('front'))
             else:
                 return redirect("/login")
         else:
             return render_template("signup.html")
-        # user = User.login(username, password)
-        # if user:
-        #     self.login(user)
-        #     return redirect("/")
-        # else:
-        #     return redirect("/signup")
     else:
         return render_template('login.html')
 
 
+
+
+@app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    session.pop("username", None)
+    return redirect("/")
+
+    
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 
@@ -227,7 +163,7 @@ def valid_pass(name, password, hashed):
     return hashed == make_pass_hash(name, password, salt)
 
 
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/signup/", methods=["GET", "POST"])
 def signup():
 
     if request.method == "POST":

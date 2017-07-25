9cb041a4ccf1ffe480d226bdb7647c315c6480e4
Ian Agpawa <agpawaji@gmail.com>
2017-07-24 20:47:51 -0400

    refactor: Change project name to drop down list

---
+++ listing.py
@@ -0,0 +1,8 @@
+website = {
+    'Tech Blog': "https://chronicle-170419.appspot.com",
+    "Music Catalog": "http://ec2-54-236-32-135.compute-1.amazonaws.com",
+    "Portfolio": "https://ian-agpawa.appspot.com",
+    "Weather Forecast": "https://weather-169903.appspot.com",
+    "TacoHub": "https://tacohub-155203.appspot.com" ,
+    "Music Blog": "https://synchronicity-collective.appspot.com"
+}
